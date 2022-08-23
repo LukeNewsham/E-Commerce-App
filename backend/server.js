c11ea8cff86949656857f6a0ec6f0ebe8ca2d879
LukeNewsham <luke.newsham@outlook.com>
2022-08-23 16:22:16 +0100

    Add in all database routers

--- backend/server.js
+++ backend/server.js
@@ -9,11 +9,25 @@ app.use(express.json())
 
 //ROUTES
 
-//User routes
+//users
 const userRouter = require('./routes/users.js') //variable to connect to user routes
 app.use('/users', userRouter); //connecting variable to all URL starting with users
 
+//user_details
+const userDetailsRouter = require('./routes/user_details.js')
+app.use('/user_details', userDetailsRouter);
 
+//products
+const productsRouter = require('./routes/products.js')
+app.use('/products', productsRouter);
+
+//basket
+const basketRouter = require('./routes/basket.js')
+app.use('/basket', basketRouter);
+
+//orders
+const orderRouter = require('./routes/orders.js')
+app.use('/orders', orderRouter);
 
 
 
